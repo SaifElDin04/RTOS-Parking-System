86e131da4fbb1f501ba154a8128fd5ff11249cbc
MalZaiter <malossama.business@gmail.com>
2026-05-14 12:22:50 +0300

    Add led.c

--- led.c
+++ led.c
@@ -2,14 +2,13 @@
 #include "config.h"
 #include "tm4c123gh6pm.h"
 
-
 void LED_Set(uint32_t mask, bool on)
 {
-    /* TODO */
+    if (on) GPIO_PORTF_DATA_R |=  (mask & LED_MASK);
+    else    GPIO_PORTF_DATA_R &= ~(mask & LED_MASK);
 }
-
 
 void LED_AllOff(void)
 {
-    /* TODO */
+    GPIO_PORTF_DATA_R &= ~LED_MASK;
 }
